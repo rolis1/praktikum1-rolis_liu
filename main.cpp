721a7f11f452e79f54a80534d17c9eefd1ba7cca
rolis1 <rolisliu0@gmail.com>
2023-10-14 14:20:00 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for ( int i = 0 ; i < 100 ; i++) {
+        cout << "-";
+    }
+    cout << "\nNama : Rolis Liu\n\n";
+    cout << "NPM : 2310631170117\n\n";
+    cout << "Kelas : 1C Inofrmatika\n\n";
+    cout << "Cita - cita : Data Analyst\n\n";
+    cout << "Motivasi Masuk Informatika : Suka berlogika dan coding\n";
+    for ( int i = 0 ; i < 100 ; i++) {
+        cout << "-";
+    }
+    return 0;
+}
